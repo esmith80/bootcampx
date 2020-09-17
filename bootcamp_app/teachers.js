f5617434c11d69779e9f4e89447b58039eca72a9
Edward Smith <edward.m.smith80@gmail.com>
2020-09-17 13:43:41 +0000

    refactored queries to guard against sql injection - teachers.js query changed to allow LIKE for the cohort parameter (it is still case-sensitive though)

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'vagrant'
 });
+// this query will not work for the user if they enter a value which returns no results (case-sensitive)
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `
+  SELECT cohorts.name as cohort, teachers.name as teacher
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON assistance_requests.student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name;
+  `;
+const values = [`%${cohortName}%`];
 
-pool.query(`
-SELECT cohorts.name as cohort, teachers.name as teacher
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON assistance_requests.student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name
-`)
+pool.query(queryString, values)
 .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
